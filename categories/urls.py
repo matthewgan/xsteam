d1f2e145573d1769606c0954ef67ab279f2ea04b
Matthew Gan <matthewgan@126.com>
2018-08-19 00:47:56 +0800

    add CategoryCourseListView

--- categories/urls.py
+++ categories/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import CategoryListView
+from .views import CategoryListView, CategoryCourseListView
 
 
 urlpatterns = [
     url(r'^$', CategoryListView.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', CategoryCourseListView.as_view()),
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,8 +1,26 @@
+from django.http import Http404
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
+from courses.models import Course
+from courses.serializers import CourseSerializer
 
 
 class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class CategoryCourseListView(APIView):
+    def get_objects(self, pk):
+        try:
+            return Course.objects.filter(categoryID=pk)
+        except Course.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        courses = self.get_objects(pk)
+        serializer = CourseSerializer(courses, many=True)
+        return Response(serializer.data)
