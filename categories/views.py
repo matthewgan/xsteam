d2e0b33b642c9cafba826b03c1f4a43ba5931f1b
roxasw <roxas.wang@gmail.com>
2018-09-09 23:35:54 +0800

    edit
    - 修改目录获取方式为name

--- categories/views.py
+++ categories/views.py
@@ -16,7 +16,7 @@ class CategoryListView(generics.ListAPIView):
 class CategoryCourseListView(APIView):
     def get_objects(self, pk):
         try:
-            return Course.objects.filter(categoryID=pk)
+            return Course.objects.filter(name=pk)
         except Course.DoesNotExist:
             return Http404
 
